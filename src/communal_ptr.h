22f66056cf0d6d7575ff4a855debc5f12423d818
Clayton Knittel <cknit1999@gmail.com>
2024-07-03 15:29:44 -0700

    test suite and basic scaffolding

---
+++ src/communal_ptr.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace paige {
+
+// TODO: Define type `CommunalPtr`, which takes a single template type argument
+// and has the same interface as `std::shared_ptr`.
+
+}  // namespace paige
