831acb6029cff720ff78200644d8b930ddba91eb
Paige Knittel <plknit00@gmail.com>
2024-07-18 12:00:49 -0700

    passing 10 of 14

--- src/communal_ptr.h
+++ src/communal_ptr.h
@@ -10,30 +10,32 @@ namespace paige {
 template <typename T>
 class CommunalPtr {
  private:
-  std::vector<CommunalPtr*>* references;
-  T* value;
+  std::vector<CommunalPtr*>* references_;
+  T* value_;
 
  public:
   // default constructor
-  explicit CommunalPtr() {
-    references->push_back(this);
-  }
+  explicit CommunalPtr() : references_(nullptr), value_(nullptr) {}
 
   // non default constructor
   explicit CommunalPtr(T* val) {
-    value = val;
-    references = new std::vector<CommunalPtr*>;
-    references->push_back(this);
+    value_ = new T;
+    value_ = val;
+    references_ = new std::vector<CommunalPtr*>;
+    references_->push_back(this);
   }
 
   // copy constructor
-  CommunalPtr(const CommunalPtr& ptr) : value(ptr.value) {
-    references->push_back(this);
+  CommunalPtr(const CommunalPtr& ptr)
+      : references_(ptr.references_), value_(ptr.value_) {
+    if (value_ != nullptr) {
+      references_->push_back(this);
+    }
   }
 
   void swap(CommunalPtr& ptr1) {
-    std::swap(ptr1.references, this->references);
-    std::swap(ptr1.value, this->value);
+    std::swap(ptr1.references_, this->references_);
+    std::swap(ptr1.value_, this->value_);
   }
 
   // overload assignment opperator
@@ -44,31 +46,35 @@ class CommunalPtr {
 
   // overload dereference opperator
   T& operator*() {
-    return *value;
+    return *value_;
   }
 
   // overload arrow opperator
   T* operator->() {
-    return value;
+    return value_;
   }
 
   T* get() {
-    return value;
+    return value_;
   }
 
   bool empty() {
-    return references->size() == 0;
+    return references_ == nullptr;
+    // return references_->size() == 0;
   }
 
   int use_count() {
-    return references->size();
+    if (references_ == nullptr) {
+      return 0;
+    }
+    return references_->size();
   }
 
   // destructor
   ~CommunalPtr() {
     if (empty()) {
-      delete value;
-      delete references;
+      delete value_;
+      delete references_;
     }
   }
 };
